5403b2dd40247711cba037b3f3f6f25d99021bd2
Nick Charlton <hello@nickcharlton.net>
2013-03-24 21:50:06 +0000

    Removes the test IOIO code to begin replacing with a Singleton.

--- Bio-OS/src/org/idat/bio_os/MainActivity.java
+++ Bio-OS/src/org/idat/bio_os/MainActivity.java
@@ -1,14 +1,9 @@
 package org.idat.bio_os;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ToggleButton;
-
-import ioio.lib.api.DigitalOutput;
-import ioio.lib.api.exception.ConnectionLostException;
-import ioio.lib.util.BaseIOIOLooper;
-import ioio.lib.util.IOIOLooper;
-import ioio.lib.util.android.IOIOActivity;
 
 /**
  * This is the main activity.
@@ -19,7 +14,7 @@ import ioio.lib.util.android.IOIOActivity;
  * @author Nick Charlton <hello@nickcharlton.net>
  *
  */
-public class MainActivity extends IOIOActivity {
+public class MainActivity extends Activity {
 	/** The main button */
 	private ToggleButton button_;
 
@@ -32,6 +27,27 @@ public class MainActivity extends IOIOActivity {
 		setContentView(R.layout.activity_main);
 		button_ = (ToggleButton)findViewById(R.id.button);
 	}
+	
+	/**
+	 * Called when the application is resumed and first started.
+	 * 
+	 * This is the first call to the Singleton, setting up the hardware
+	 * access.
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
+	/**
+	 * Called when the application is paused.
+	 * 
+	 * Tell the Singleton that it should stop accessing the hardware.
+	 */
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
 
 	/**
 	 * Creates the menu which is invoked by a menu button.
@@ -43,55 +59,6 @@ public class MainActivity extends IOIOActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-	
-	/**
-	 * The thread where the IOIO activity happens.
-	 * 
-	 * setup() is called when the device is connected. loop() is then 
-	 * called continuously.
-	 */
-	class Looper extends BaseIOIOLooper {
-		/** The default LED. */
-		private DigitalOutput led_;
-		
-		/**
-		 * Called every time a connection with the IOIO is established.
-		 * 
-		 * @throws ConnectionLostException
-		 * 			When the IOIO connection is lost.
-		 */
-		@Override
-		protected void setup() throws ConnectionLostException {
-			led_ = ioio_.openDigitalOutput(0, true);
-		}
-		
-		/**
-		 * Called continuously when connected.
-		 * 
-		 * @throws ConnectionLostException
-		 * 			When the IOIO connection is lost.
-		 */
-		@Override
-		public void loop() throws ConnectionLostException {
-			led_.write(!button_.isChecked());
-			
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				
-			}
-		}
-	}
-	
-	/**
-	 * A method to build up the IOIO thread.
-	 * 
-	 * @return IOIOLooper instance.
-	 */
-	@Override
-	protected IOIOLooper createIOIOLooper() {
-		return new Looper();
 	}
 
 }
