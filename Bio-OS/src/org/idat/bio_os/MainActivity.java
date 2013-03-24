782d05c989ece81d97366963b649e5086438d63e
Nick Charlton <hello@nickcharlton.net>
2013-03-24 21:56:36 +0000

    Replaces the settings menu item.
    And adds the ability to respond to menu items.

--- Bio-OS/src/org/idat/bio_os/MainActivity.java
+++ Bio-OS/src/org/idat/bio_os/MainActivity.java
@@ -3,6 +3,7 @@ package org.idat.bio_os;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ToggleButton;
 
 /**
@@ -58,7 +59,24 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
 		return true;
+	}
+	
+	/**
+	 * Responds to menu taps.
+	 * 
+	 * @return true if there is an action to be performed.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_about:
+			// show the about activity
+			return true;
+		}
+		
+		return false;
 	}
 
 }
