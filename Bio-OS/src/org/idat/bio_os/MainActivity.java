c01f608db4b1e36cee706164f939daedf86bc964
Nick Charlton <hello@nickcharlton.net>
2013-03-28 01:04:11 +0000

    Adds the About Activity.
    This will show connection status and allow the user to test the
    connection by flicking the yellow light on and off.

--- Bio-OS/src/org/idat/bio_os/MainActivity.java
+++ Bio-OS/src/org/idat/bio_os/MainActivity.java
@@ -4,6 +4,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -127,10 +128,12 @@ public class MainActivity extends Activity {
 		switch (item.getItemId()) {
 		case R.id.action_about:
 			// show the about activity
+			Intent intent = new Intent(this, AboutActivity.class);
+			this.startActivity(intent);
 			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-		
-		return false;
 	}
 	
 	/**
